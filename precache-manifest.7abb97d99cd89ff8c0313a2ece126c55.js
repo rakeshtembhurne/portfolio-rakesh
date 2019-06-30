0ae9266ef62df886cf662fa3c1e2f8614f3c7099
Rakesh Tembhurne <tj2point0@gmail.com>
2019-07-01 05:02:18 +0530

    Updates

---
+++ precache-manifest.7abb97d99cd89ff8c0313a2ece126c55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f648d108cb1ec1f77b0acc04875d107",
+    "url": "/portfolio-rakesh/index.html"
+  },
+  {
+    "revision": "fa52854a9d1bbf91ea52",
+    "url": "/portfolio-rakesh/static/css/main.4e4ca929.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio-rakesh/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fa52854a9d1bbf91ea52",
+    "url": "/portfolio-rakesh/static/js/main.1cbf6388.chunk.js"
+  },
+  {
+    "revision": "f7b82caacfa7e3825e0f",
+    "url": "/portfolio-rakesh/static/js/runtime~main.d5c03c62.js"
+  }
+]);
